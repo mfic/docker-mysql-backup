1449d0f3ac0169a280b0579c924969c7f0280ad0
Jan Quest <janquest@gmail.com>
2019-11-09 23:57:24 +0100

    --amend

---
+++ database.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+
+function backup(){
+	# Backup
+	docker exec $container /usr/bin/mysqldump -u $user --password=$passwd $database > $file$current_time.sql
+}
+
+function restore() {
+	# Restore
+	cat $file | docker exec -i $container /usr/bin/mysql -u $user --password=$passwd $database
+}
+
+# Call definitions
+
+if [[ -f $1 ]]
+then
+	source $1
+else
+	echo "File not found. Please define env file."
+	exit 1
+fi
+
+case "$2" in
+	backup)
+		file="./db/backup-$container-$current_time.sql"
+		echo "Backup $container $passwd $database $file"
+		;;
+
+	restore)
+		file=$3
+		if [[ -f "$file" ]]
+		then
+			echo "Restore $container $passwd $database $file"
+		else 
+			echo "File not found. Please specify correct path to the MySQL backup you wish to restore."
+		fi
+		;;
+
+	*)
+		echo "Use either backup or restore"
+		exit 1
+esac
